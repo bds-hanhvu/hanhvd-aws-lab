57975b0b7ea23c71a4c2f9096dd321bc25d365bf
bds-hanhvd <hanhvd@batdongsan.com.vn>
2024-01-16 11:17:38 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-	terraform {
-	  backend "s3" {
-	    bucket = "hanhvd-atlantis"
-	    #key    = "path/to/my/terraform.tfstate"
-	    key    = "poc-atlantis/terraform.tfstate"
-	    region = "ap-southeast-1"
-	  }
+terraform {
+	backend "s3" {
+		bucket = "hanhvd-atlantis"
+		#key    = "path/to/my/terraform.tfstate"
+		key    = "poc-atlantis/terraform.tfstate"
+		region = "ap-southeast-1"
 	}
+}
