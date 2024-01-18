f946a56de539ce2d810e089ebe08cbde79177bd4
bds-hanhvd <hanhvd@batdongsan.com.vn>
2024-01-18 15:56:38 +0700

    add

--- providers.tf
+++ providers.tf
@@ -12,9 +12,9 @@ provider "aws" {
 
 
 provider "aws" {
-  region = "ap-southeast-1"
-  alias = "vncross-user"
-  profile = "vncross-user"
+  #region = "ap-southeast-1"
+  #alias = "vncross-user"
+  #profile = "vncross-user"
   assume_role {
     role_arn = "arn:aws:iam::987549667347:role/atlantis-assume-role"
     session_name = "vncross-user"
